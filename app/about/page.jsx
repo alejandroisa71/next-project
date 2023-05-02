206939fd4a0fb60ac4c3d2d2e26b65cf36d04461
unknown <alejandroisa71@gamail.com>
2023-05-02 18:42:33 -0300

    mi first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1> About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in
+        dicta facere. Quod dolorem ut modi dolore fugiat quidem quasi tenetur
+        rerum perferendis, repellendus non hic, provident cum delectus vero
+        explicabo nostrum laboriosam aliquam aspernatur, perspiciatis ipsum quo.
+        Deleniti, odit mollitia? Neque non, laudantium maxime placeat quaerat
+        doloribus id blanditiis temporibus possimus omnis ab quo earum quas,
+        quisquam ducimus! Maiores, expedita nesciunt officia, veritatis debitis
+        ad, magnam vitae minus a commodi nemo omnis eius dolores? Adipisci
+        laborum quam dolor distinctio voluptate possimus nemo, accusamus eaque
+        officiis animi aperiam consequatur ipsum numquam sed non quis obcaecati,
+        unde quia culpa magnam vitae.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <h1>Services Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed illo
+        accusantium voluptatibus aut quam eligendi dolore rerum, error nostrum
+        totam et vel assumenda laudantium reprehenderit ratione minus aliquam
+        iusto, in veniam. Iusto facere doloribus culpa pariatur placeat ex autem
+        ut minus, tenetur repellat asperiores quos fuga, neque, praesentium
+        aliquam in!
+      </p>
+    </div>
+  );
+};
+
+export default ServicesPage;
