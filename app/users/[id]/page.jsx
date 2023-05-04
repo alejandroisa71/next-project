2297050be10e30a9e5bf5f8e4eb86810d89c87d2
unknown <alejandroisa71@gamail.com>
2023-05-04 19:09:31 -0300

    Se agregan estilos con boostrap

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,27 +1,33 @@
 import { requestToBodyStream } from "next/dist/server/body-streams";
 
+const getUser = async (id) => {
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
+};
 
-async function getUser(id){
-  const res= await fetch(`https://reqres.in/api/users/${id}`)
-  const data = await res.json()
-  return data.data
-}
-
-async function UserPage({ params }) {
-
+const UserPage = async ({ params }) => {
   const user = await getUser(params.id);
   // console.log(data);
 
   return (
-    <div>
-      <h1>Detail</h1>
-      <div>
-        <img src={user.avatar} alt={user.email}/>
-        <h3>{user.id} {user.first_name} {user.last_name}</h3>
-        <p>{user.email}</p>
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+       <div className="card">
+       <div className="card-header text-center">
+       <img src={user.avatar} alt={user.email} />
+
+       </div>
+       <div className="card-body text-center">
+          <h3>
+            {user.id} {user.first_name} 
+          </h3>
+          <p>{user.email}</p>
+       </div>
+       </div>
       </div>
     </div>
   );
-}
+};
 
 export default UserPage;
--- components/Users.jsx
+++ components/Users.jsx
@@ -1,27 +1,34 @@
 "use client";
-import {useRouter} from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 function Users({ users }) {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    return (
-        <ul>
-            {users.map((user) => (
-                <li key={user.id}
-                    onClick={() => {
-                        router.push(`/users/${user.id}`)
-                    } }
-                >
-                    <div>
-                        <h5>{user.id} {user.first_name} {user.last_name}</h5>
-                        <p>{user.email}</p>
-                        <img src={user.avatar} alt={user.email} />
-                    </div>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="list-group">
+      {users.map((user) => (
+        <li
+          className="list-group-item d-flex justify-content-between align-items-start list-group-item-action"
+          key={user.id}
+          onClick={() => {
+            router.push(`/users/${user.id}`);
+          }}
+        >
+          <div>
+            <h5>
+              {user.id} {user.first_name} {user.last_name}
+            </h5>
+            <p>{user.email}</p>
+          </div>
+          <img
+            src={user.avatar}
+            alt={user.email}
+            style={{ borderRadius: "50%" }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Users
+export default Users;
